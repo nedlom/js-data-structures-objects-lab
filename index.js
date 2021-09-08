eede799d19b80da29fe1aa328056bf9f29453ba5
Nick <nelom3@gmail.com>
2021-09-08 07:34:12 -0700

    commit tree 64b734e230c04382ac07826d4f9a98c77169e4a1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const driver_clone = Object.assign({}, driver);
+  delete driver_clone[key];
+  return driver_clone;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
